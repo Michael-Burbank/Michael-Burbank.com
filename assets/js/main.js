a4c5be10bbda752f791a73e73a120c8652ef742c
Michael Burbank <burbank2@yahoo.com>
2024-08-11 17:26:32 -0500

    Change background color

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,13 @@
 	/**
 	 * Easy event listener function
 	 */
+
+
 	const on = (type, el, listener, all = false) => {
 		let selectEl = select(el, all);
-		if (selectEl) {
-			if (all) {
+
+		if(selectEl){
+			if(all){
 				selectEl.forEach((e) => e.addEventListener(type, listener));
 			} else {
 				selectEl.addEventListener(type, listener);
