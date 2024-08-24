0dc2756274cd8c408fe182d870b650bf64501a10
Michael Burbank <burbank2@yahoo.com>
2024-08-24 13:18:53 -0500

    Modify CSS

---
+++ assets/js/test.js
@@ -0,0 +1,21 @@
+let oldArray = [1, 4, 2, 1, 6];
+
+// GOAL: Return sorted array in ascending order. 
+
+let newArray = [];
+let position = 0;
+
+function sort(oldArray){
+    for(let i = 1; i <= oldArray.length; i++){
+        for(let j = 0; j <= i - 1; j++){
+            if(oldArray[i] < oldArray[j]){
+                
+            }
+        }
+    }
+
+
+
+
+
+};
