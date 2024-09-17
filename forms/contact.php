54c48b98d6d6fe90ce84ada0cb6b9a19e9ebc95c
Michael Burbank <burbank2@yahoo.com>
2024-09-17 12:57:10 -0700

    Resize #aboutImg

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,4 @@
 <?php
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'mike.w.burbank@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -15,6 +14,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  */
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
