fc4aa837edf9451a5fb483ca7ffc885bb5c1ba12
Michael Burbank <burbank2@yahoo.com>
2024-10-06 13:56:20 -0700

    Test smtp connection.

--- forms/contact.php
+++ forms/contact.php
@@ -1,8 +1,9 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+
+/**
+ * Requires the "PHP Email Form" library
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
 
 // TODO!: Use below for Contact.php Inquiry Web Form. 
 //TODO!: Implement Google restAPI / GMAIL SMTP?
@@ -15,21 +16,21 @@
 // TODO!: https://www.namecheap.com/support/knowledgebase/article.aspx/10038/31/how-to-configure-a-contact-form-with-us/#smtp
 
 
-  $receiving_email_address = '#';
+$receiving_email_address = '#';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+  include($php_email_form);
+} else {
+  die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
+
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
 
 
 // TODO!: Finish setting up SMTP prot. requirements for GMAIL PHP Web Contact form. 
@@ -37,17 +38,16 @@
 // TODO!: Implement Google API => Mail - Contact.php Web form - Inquiry => GMAIL
 
 // TODO!: SMTP to send emails. Enter correct SMTP credentials
-  // $contact->smtp = array(
-  //   'host' => 'smtp.gmail.com', // TODO: <= SMTP
-  //   'username' => 'burbank536@gmail.com',   // TODO: <= Email add.
-  //   'password' => '#_ENTER WHEN_READY',     // TODO: <= Email pass.
-  //   'port' => '587'          // TODO: <= Default Port #. SSL
-  // );
-  
+$contact->smtp = array(
+  'host' => 'smtp.gmail.com', 
+  'username' => 'burbank536@gmail.com',   
+  'password' => 'yxbxfrzotwvnrpsx',     
+  'port' => '465'
+);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
+
+echo $contact->send();
