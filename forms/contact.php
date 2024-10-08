b05fa96b80049a46b944c3b6379145a92c04c6aa
Michael Burbank <burbank2@yahoo.com>
2024-10-07 19:00:29 -0700

    Hash smtp password. Add to .gitignore.

--- forms/contact.php
+++ forms/contact.php
@@ -16,14 +16,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp-mail.outlook.com',
+    'username' => 'admin@michael-burbank.com',
+    'password' => getenv('SMTP_PASSWORD'), 
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
